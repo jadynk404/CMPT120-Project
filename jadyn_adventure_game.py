4105b3d8455356996fed772ccbf13b1bf6994a21
Jadyn Kennedy <jadyn.kennedy1@marist.edu>
2020-02-29 12:17:44 -0500

    added loop and directionality, new two locals still and global list

--- jadyn_adventure_game.py
+++ jadyn_adventure_game.py
@@ -1,88 +1,239 @@
 # Name: Jadyn Kennedy
 # Date: 2/10/2020
-# Citation: resources on iLearn, Emmanuel Batista
+
+
+e = ("Press enter to continue...")
+g = ("GPA:")
+name = input("Please enter your name: ")
+year = input("Please enter what year you are (freshman/sophomore/junior/senior): ")
+invalid = "I'm sorry. That is not a valid command."
+
+
+import time
+import sys
+
+
+
+             
+def someFunc(function):   #added parameter          
+    function()
 
 def copyright(variable):
     print(variable)
-    
 
-def print1():
+
+def Quit():
+    print("ok. goodbye")
+    sys.exit()
+    
+def loop():
+    x = 1
+    while x == 1:
+        prompt = "Where would you like to go from here?\n"
+        choice = input(prompt)
+        invalid = "That is not a valid command. Enter 'help' for help"
+        Help = "Please suggest either north, south, east, west, or quit.\n"
+        north = "you go north"
+        south = "you go south"
+        east = "you go east"
+        west = "you go east"
+        
+        
+        
+
+        choice = choice.lower()
+        if choice[0] == "n":
+            x = 0
+            print(north)
+
+        elif choice[0] == "s":
+            x = 0
+            print(south)
+        elif choice[0] == "e":
+            x = 0
+            print(east)
+
+        elif choice[0] == "w":
+            x = 0
+            print(west)
+        elif choice[0] == "h":
+            x = 1
+            print(Help)
+        elif choice [0] == "q":
+            Quit()
+        else:
+            x = 1
+            print(invalid)
+            
+
+    
+    return loop
+
+
+
+def Intro():
     p1 = 'Help the STEM major find her motivation!'
 
     print(p1)
-    print() # print blank space 
+    print() # print blank space
+    time.sleep(1)
 
-    e = ("Press enter to continue...")
-    g = ("GPA:")
-    gpa = float(4.0)
-    name = input("Please enter your name: ")
-    year = input("Please enter what year you are (freshman/sophomore/junior/senior): ")
-    p2 = "Welcome to {} orienation,{}! You begin your year year, ready and eager to learn!".format(year, name) #this fixes the format 
+    
+    p2 = "Welcome to {} orientation, {}! You begin your {} year, ready and eager to learn!".format(year, name, year) #This fixes the formatting of text
     p3 = "... Two weeks later ..."
-    p4 = "Oh no! You can't seem to find your motivation to continue! Where should you look?"
-    p5 ="You look through all the drawers in your room, making a mess in the process. There's nothing here. You know you have a project due in your Programming class in an hour, but you decide to clean up your mess instead."
-    p6 = "You were late to class... with an incomplete project."
-    p7 = "Congratulations,{}! After your teacher scolded you for your project, you found a pinch of motivation on the floor and finally finish that project! You turn it in 2 days late and luckily your teacher takes pity on you and accepts it.".format(name)
-    p8 = "You feel relieved to see your grades back up and decide to give yourself a nice break as a reward so you decide to skip your classes for the next day."
-    p9 = "You start seeing assignments pile up in your peripheral vision as you watch netflix. You know you should start them, but your motivation has once again run out."
-    p10 = "Press enter to look for some more!"
-    p11 = "As you look start searching, you come across an email saying: '{}', Don't forget to keep your grades up so you don't lose your full ride scholarship.'".format(name)
-    p12 = "Congratulations! That should be enough motivation to last you until you graduate!"
-    p13 = "You complete all of your assignments and finish the semester strong."
-    p14 = "You have won! Please play again!"
-   
 
     
 
     
     print()
     print(p2)
-    print(g, gpa)
+    print(g, 4.0)
     input(e)
     print(p3)
 
-    print(p4)
-    input(e)
-    print(p5)
+def printsub1():
+    p4sub1 = "Oh no! You can't seem to find your motivation to continue!"
+    print(p4sub1)
+
+def print1():
+    x = 1
+    while x==1:
+        
+        p4 = "Where should you look: through your drawers or in the closet?\n"
+        p51 ="You look through all the drawers in your room, making a mess in the process. There's nothing here." 
+        p52 = "Your scavenge through your closet, throwing all of your clothes onto the floor. There's nothing here." 
+        suggest1 = "Please suggest either drawers or closet.\n"
+        choice1 =input(p4)
+
+        choice1 = choice1.lower()
+
+        if choice1[0] == "d":
+            x = 0
+            print(p51)
+        elif choice1[0] == "c":
+            x = 0
+            print(p52)
+
+            
+        else:
+            print(invalid)
+            tryagain = input(suggest1)
+            tryagain = tryagain.lower() #allows reader to input capital or lower case answers
+            if tryagain[0] == "d":
+                x = 0
+                print(p51)
+            elif tryagain[0] == "c":
+                x = 0
+                print(p52)
+            else:
+                x=1
+                
+            
+    
+        input(e)
+
+def printincomplete():
+    p6 = "You were late to class... with an incomplete project."
+   
 
     print(p6)
-    print(g, gpa-.5)
+    print(g, 3.5)
     input(e)
 
-    print(p7)
-    print(g, gpa-.2)
+
+def printwork():
+    work = "You made it to class with a half finished project." 
+
+    print(p6)
+    print(g, 3.7)
     input(e)
+
+    
+def print2():
+    x = 1
+    while x==1:
+        project = "You have your Computer Programming game due in an hour and you haven't started it yet but your roommate has already yelled at you twice this week to clean the room. Do you try to work on it or clean up the mess you made before your roommate gets home?\n"
+        suggest2 = "Please suggest either clean or work.\n"
+        choice2 = input(project)
+
+        choice2 = choice2.lower()
+        if choice2[0] == "c":
+            x = 0
+            printincomplete()
+
+        elif choice2[0] == "w":
+            x = 0
+            printwork()
+
+        
+        else:
+            x = 1
+            
+            tryagain = input(suggest2)
+            tryagain = tryagain.lower()
+            if tryagain[0] == "w":
+                x = 0
+                print(printwork)
+            elif tryagain[0] == "c":
+                x = 0
+                print(printincomplete)
+            else:
+                x = 1
+            
+
+
+
+def print3():
+
+    p8 = "You feel relieved to see your grades back up and decide to give yourself a nice break as a reward so you decide to skip your classes for the next day."
+    p9 = "You start seeing assignments pile up in your peripheral vision as you watch netflix. You know you should start them, but your motivation has once again run out."
+    
 
     print(p8)
-    print(g, gpa-1.0)
+    print(g, 3.0)
     input(e)
 
     print(p9)
-    input(p10)
+    loop()
+
+def print4():
+    p11 = "As you look start searching, you come across an email saying: '{}, Don't forget to keep your grades up so you don't lose your full ride scholarship.'\n".format(name)
+    p12 = "Congratulations! That should be enough motivation to last you until you graduate!"
+    p13 = "You complete all of your assignments and finish the semester strong."
+
     print(p11)
-    input(e)
     print(p12)
     input(e)
+
+def print5():
     print(p13)
-    print(g, gpa)
+    print(g, 4.0)
+
+def print5():
+    p14 = "You have won! Please play again!"
 
     print(p14)
-    
-
 
     
 def main():
-    print1()
 
-    c = "This game was created by Jadyn Kennedy 2/10/2020" # copyright
-    copyright(c)
+        someFunc(Intro)
+        someFunc(printsub1) 
+        someFunc(print1)
+        someFunc(print2)
+        someFunc(print3)
+        someFunc(print4)
+        someFunc(print5)
+
+#To do: Add two more locals, create choices for each local, change the GPA depending on which route taken
+
 
     
-
-
 
 main()
     
     
+
+
 
 
